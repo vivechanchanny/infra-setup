888d27141e0b4e11a8aa56827b38007c9a8e350b
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-08-29 16:16:28 +0530

    testing cicd setup with Manuel initiation

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
-sudo su
 
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://codedeploystack-webappdeploymentbucket-np3h39n1a6g9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
