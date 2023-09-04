e27cc7fa4d7ad55d190604f05df456ef87f88daf
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-09-04 13:33:17 +0530

    check blue green deployment

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1> <h2> poc for github actions </h2>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1> <h2> poc for github actions </h2> <h3> blue/green working file</h3>";
 }
 }
