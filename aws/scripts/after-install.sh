7038f3f21b3037469bf30dc5353fbe55a46e30d7
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-09-04 11:27:46 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-np3h39n1a6g9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://cicd-poc-webappdeploymentbucket-1bt1mchefb3oi/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
